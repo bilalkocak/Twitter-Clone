0cb841548a7eaa7c31ff609222c8a9f06fbcc8c4
Bilal Koçak <kocakbilal04@gmail.com>
2018-03-31 12:30:25 +0300

    php klasöründen vaz geçtim

--- index.php
+++ index.php
@@ -1,4 +1,4 @@
-<?php include('php/anasayfacerceveust.php')  ?>
+<?php include('anasayfacerceveust.php')  ?>
 
 <div class="LogOut-sag">                  
     <div class="LogOut-sag-signup">
@@ -7,7 +7,7 @@
         </div>
         <h1 class="suanda">Şu anda dünyada neler olup bittiğini gör</h1>
         <h2 class="katil"><?php if($_GET['hata']=='sifre'){ echo 'Şifre veya mail hatalı !!!';} else {echo "Twitter'a giriş yap.";} ?></h2>
-        <form action="php/girisyap.php" method="post">
+        <form action="girisyap.php" method="post">
             <div class="signup-posta">
                 <input type="email" class="txtbox-kayit" name="uye_mail"  placeholder="E-posta">
             </div>
@@ -17,11 +17,11 @@
             <div class="signup-button">
                 <input type="submit" class="btn-basla" value="Giriş yap">
                 <div>
-                    <p>Hesabın yok mu? <a href="php/kayitol.php" class="giris">Kayıt Ol</a></p>
+                    <p>Hesabın yok mu? <a href="kayitol.php" class="giris">Kayıt Ol</a></p>
                 </div>
             </div>
         </form>
     </div>
 </div>
 
-<?php include('php/anasayfacercevealt.php')  ?>
+<?php include('anasayfacercevealt.php')  ?>
