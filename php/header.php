375ae1a12a981cb73aa2e1f74717a26bc0acaae5
Bilal Koçak <kocakbilal04@gmail.com>
2018-03-31 04:18:16 +0300

    header css düzenlendi

--- php/header.php
+++ php/header.php
@@ -9,27 +9,28 @@
     </head>
     <body>
         <div class="header">
-            <div class="header-sol">
-                <div class="header-sol-buton">
-                    <a href="">Anasayfa</a>
+            <div class="container">
+                <div class="header-sol">
+                    <div class="header-sol-buton">
+                        <a href="">Anasayfa</a>
+                    </div>
+                    <div class="header-sol-buton">
+                        <a href="">Profil</a>
+                    </div>
                 </div>
-                <div class="header-sol-buton">
-                    <a href="">Profil</a>
+                <div class="header-logo">
+                    <a href=""><img src="images/twitter.png" class="logo"></a>
+                </div>
+                <div class="header-sag">
+                    <form action="" class="form">
+                        <input type="text" placeholder="Twitter'da Ara" class="header-ara">
+                        <input type="submit" class="ara-buton" value="Ara">
+                    </form>
+                    <div class="header-sag-menu">
+                        <a href=""> <img src="images/twitter.png" class="header-sag-menu-icon"> </a>
+                    </div>
                 </div>
             </div>
-            <div class="header-logo">
-                <a href=""><img src="images/twitter.png" class="logo"></a>
-            </div>
-            <div class="header-sag">
-                <form action="" class="form">
-                    <input type="text" placeholder="Twitter'da Ara" class="header-ara">
-                    <input type="submit" class="ara-buton" value="Ara">
-                </form>
-                <div class="header-sag-menu">
-                    <a href=""> <img src="images/twitter.png" class="header-sag-menu-icon"> </a>
-                </div>
-            </div>
-            
         </div>
     </body>
 </html>
