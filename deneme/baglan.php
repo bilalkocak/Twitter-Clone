c7c03fb58620041447121324f9a035aa90aa0d2a
Bilal Koçak <kocakbilal04@gmail.com>
2018-03-31 03:46:32 +0300

    deneme klasörü silindi

--- deneme/baglan.php
+++
@@ -1,5 +0,0 @@
-<?php 
-   //veri tabanı bağlantısı
-   $db=new PDO('mysql:host=localhost;dbname=dersler','root','f120813b');
-   
-?>
--- deneme/header.php
+++
@@ -1,2 +0,0 @@
-<a href="homepage.php">[Ana Sayfa]</a>
-<a href="index.php?sayfa=insert">[İçerik Ekle]</a> <hr> 
